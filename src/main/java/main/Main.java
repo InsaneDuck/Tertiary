ce51ac180b05a1a3dd99fc085a02afe992d8b106
Satya <39309626+InsaneDuck@users.noreply.github.com>
2022-05-02 14:03:01 +0100

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Created by JFormDesigner on Mon May 02 13:52:47 BST 2022
+ */
+
+package main;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author unknown
+ */
+public class Main extends JPanel {
+    public Main() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
